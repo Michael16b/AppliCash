4cd65e4f004705ba1fc3e97928d05e3d7a9d61a4
E23A977Z <=>
2026-01-27 23:54:26 +0100

    remove duplicate files

--- feature/login/src/main/java/fr/univ/nantes/feature/login/featureLoginModule.kt
+++
@@ -1,9 +0,0 @@
-package fr.univ.nantes.feature.login
-
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val featureLoginModule =
-    module {
-        viewModel { LoginViewModel(get()) }
-    }
