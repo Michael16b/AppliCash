fa9071b7bd8488e725bba708833af57d7972a771
E23A977Z <=>
2026-02-06 14:49:31 +0100

    Mise à jour des imports Koin

--- feature/expense/src/main/java/fr/univ/nantes/feature/expense/di/featureExpenseModule.kt
+++ feature/expense/src/main/java/fr/univ/nantes/feature/expense/di/featureExpenseModule.kt
@@ -1,7 +1,7 @@
 package fr.univ.nantes.feature.expense.di
 
 import fr.univ.nantes.feature.expense.ExpenseViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.*
 import org.koin.dsl.module
 
 val featureExpenseModule = module {
--- feature/login/src/main/java/fr/univ/nantes/feature/login/FeatureLoginModule.kt
+++ feature/login/src/main/java/fr/univ/nantes/feature/login/FeatureLoginModule.kt
@@ -1,6 +1,7 @@
 package fr.univ.nantes.feature.login
 
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.*
+
 import org.koin.dsl.module
 
 val featureLoginModule =
