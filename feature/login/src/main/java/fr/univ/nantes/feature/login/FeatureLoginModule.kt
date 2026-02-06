eb720fa50dc5b77bbf93f005788592b2acbbd04d
MaxenceBourdin12 <95865568+MaxenceBourdin12@users.noreply.github.com>
2026-02-06 15:40:46 +0100

    update import Koin (#13)

--- feature/login/src/main/java/fr/univ/nantes/feature/login/FeatureLoginModule.kt
+++ feature/login/src/main/java/fr/univ/nantes/feature/login/FeatureLoginModule.kt
@@ -1,6 +1,6 @@
 package fr.univ.nantes.feature.login
 
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val featureLoginModule =
