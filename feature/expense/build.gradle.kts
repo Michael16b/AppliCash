e0194ed4359e53fc1526dc22df1dc4bfde83fd6e
BESILY Michaël <Michael.besily@gmail.com>
2026-02-17 01:31:49 +0100

    add ProfileView

--- feature/expense/build.gradle.kts
+++ feature/expense/build.gradle.kts
@@ -32,12 +32,6 @@ android {
     }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-    }
-}
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
