e61d2485838ec713b635f8beb9dc7b7b96b7bb83
BESILY Michaël <Michael.besily@gmail.com>
2026-03-13 00:16:03 +0100

    import Surface from material3 in AddExpenseScreen

--- feature/expense/src/main/java/fr/univ/nantes/feature/expense/AddExpenseScreen.kt
+++ feature/expense/src/main/java/fr/univ/nantes/feature/expense/AddExpenseScreen.kt
@@ -58,6 +58,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
@@ -1617,4 +1618,3 @@ private fun AddExpenseScreenContent(
         }
     }
 }
-
